c480c3f733ea3468bfc9feeacd9b08ada0a835f5
Rita Csullag <csullagrita@gmail.com>
2020-07-16 15:06:52 +0200

    Add TwoNumbers task

---
+++ week-01/day-4/src/TwoNumbers.java
@@ -0,0 +1,16 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+
+        int a, b;
+        a= 22;
+        b= 13;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / (double)b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
