472128d47bb6ae1f843d9d1d60c254600353883a
Rita Csullag <csullagrita@gmail.com>
2020-07-16 16:11:25 +0200

    Add DefineBasicInfo task

--- week-01/day-4/src/Swap.java
+++ week-01/day-4/src/Swap.java
@@ -6,7 +6,7 @@ public class Swap {
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-        
+
         int temporary = a;
         a = b;
         b = temporary;
