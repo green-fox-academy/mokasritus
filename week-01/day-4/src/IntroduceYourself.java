cbf2e14d477bd28e221fd6d4418820d99f64a45f
Rita Csullag <csullagrita@gmail.com>
2020-07-16 14:21:39 +0200

    Add IntroduceYourself task

---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,8 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Rita");
+        System.out.println(30);
+        System.out.println(1.68);
+
+    }
+}
