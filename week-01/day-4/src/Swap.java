ee367cf533b1c22928ad052f384d7ec43fd929b3
Rita Csullag <csullagrita@gmail.com>
2020-07-16 15:53:53 +0200

    Add Swap task

---
+++ week-01/day-4/src/Swap.java
@@ -0,0 +1,17 @@
+public class Swap {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        int temporary = a;
+        a = b;
+        b = temporary;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
