223d70a9609bf2ce3b95ebdc23129a6e2db66f26
Rita Csullag <csullagrita@gmail.com>
2020-07-17 13:44:59 +0200

    Add MultiplicationTable task and Google-gf ckeckstyle

---
+++ week-01/day-5/src/MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+  public static void main(String[] args) {
+    // Create a program that asks for a number and prints the multiplication table with that number
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Tell me a number:");
+    int number = scanner.nextInt();
+
+    for (int i = 1; i < 11; i++) {
+      System.out.println(i + " * " + number + " = " + i*number);
+    }
+  }
+}
