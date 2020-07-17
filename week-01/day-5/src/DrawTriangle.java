151ed949085ed8d50ac30ec089c6cfc4968ae2c1
Rita Csullag <csullagrita@gmail.com>
2020-07-17 15:02:31 +0200

    Add DrawTriangle task

---
+++ week-01/day-5/src/DrawTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Tell me a number:");
+    int number = scanner.nextInt();
+
+    for (int i = 0; i < number +1 ; i++) {
+      for (int j = 0; j < i ; j++) {
+        System.out.print("*");
+
+      }
+      System.out.println("");
+    }
+  }
+}
