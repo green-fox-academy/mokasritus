6c87e19ad9c0abd57cfbef738ec9f15194bfe23b
Rita Csullag <csullagrita@gmail.com>
2020-07-20 17:17:40 +0200

    Add GreaterFunction task

--- week-02/day-1/src/Doubling.java
+++ week-02/day-1/src/Doubling.java
@@ -4,7 +4,7 @@ public class Doubling {
 // - Create an integer variable named `baseNum` and assign the value `123` to it
 // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
 // - Print the result of `doubling(baseNum)`
-    int baseNum = 123; 
+    int baseNum = 123;
     System.out.println(doubling(baseNum));
 
   }
