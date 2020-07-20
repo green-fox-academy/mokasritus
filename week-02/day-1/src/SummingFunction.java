5be9d35d8de9e00f48a82931d3fe9205e38ac1ea
Rita Csullag <csullagrita@gmail.com>
2020-07-20 22:59:00 +0200

    Add SummingFunction task

---
+++ week-02/day-1/src/SummingFunction.java
@@ -0,0 +1,20 @@
+
+
+public class SummingFunction {
+  public static void main(String[] args) {
+    // Create the usual class wrapper and main method on your own.
+
+    // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
+    System.out.println(sum(5));
+
+  }
+
+  public static int sum(int x) {
+    if (x == 0) {
+      return 0;
+    }
+    return x + sum(x - 1);
+  }
+}
+
