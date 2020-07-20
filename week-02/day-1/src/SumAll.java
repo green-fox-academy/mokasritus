4f8685ed857c3d9c1b917ad4616e984bb7a59acb
Rita Csullag <csullagrita@gmail.com>
2020-07-20 16:06:37 +0200

    Add SumAll task

---
+++ week-02/day-1/src/SumAll.java
@@ -0,0 +1,15 @@
+public class SumAll {
+  public static void main(String[] args) {
+    // - Create an array variable named `numbers`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `numbers`
+
+    int [] numbers = {3, 4, 5, 6, 7};
+    int sum = 0;
+    for (int num : numbers) {
+      sum = sum + num;
+    }
+
+    System.out.println(sum);
+  }
+}
