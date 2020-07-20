0e7ed921118e52dfba88feba68bb266455620dc8
Rita Csullag <csullagrita@gmail.com>
2020-07-20 14:07:18 +0200

    Add ChangeElements task

---
+++ week-02/day-1/src/ChangeElements.java
@@ -0,0 +1,16 @@
+public class ChangeElements {
+  public static void main(String[] args) {
+
+// - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4
+// - Print the fourth element
+
+    int numbers [] = {1, 2, 3, 8, 5, 6};
+    int x = 4;
+    numbers [3] = x;
+
+    System.out.println(numbers [3]);
+
+  }
+}
