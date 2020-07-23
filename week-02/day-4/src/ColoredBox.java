8a0b75043b5297a1c4b93dce3ae014f7696deb16
Rita Csullag <csullagrita@gmail.com>
2020-07-23 12:06:38 +0200

    Add ColoredBox task

---
+++ week-02/day-4/src/ColoredBox.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+  public static void mainDraw(Graphics graphics) {
+    // Draw a box that has different colored lines on each edge.
+
+    graphics.setColor(getRandomColor());
+    graphics.drawLine(140, 140, 180, 140);
+
+    graphics.setColor(getRandomColor());
+    graphics.drawLine(180, 140, 180, 180);
+
+    graphics.setColor(getRandomColor());
+    graphics.drawLine(180, 180, 140, 180);
+
+    graphics.setColor(getRandomColor());
+    graphics.drawLine(140, 180, 140, 140);
+
+  }
+
+  public static Color getRandomColor() {
+    Random random = new Random();
+    int limit = 256;
+    return new Color(random.nextInt(limit), random.nextInt(limit), random.nextInt(limit));
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
