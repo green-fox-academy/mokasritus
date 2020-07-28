fb8b06d44c90860fb1fe7ccd1e116160047e6df4
Rita Csullag <csullagrita@gmail.com>
2020-07-28 11:35:25 +0200

    Add FleetOfThings task

---
+++ week-03/day-2/src/FleetOfThings/FleetOfThings.java
@@ -0,0 +1,30 @@
+package FleetOfThings;
+
+public class FleetOfThings {
+
+  public static void main(String[] args) {
+    Fleet fleet = new Fleet();
+    // - You have the `Thing` class
+    // - You have the `Fleet` class
+    // - You have the `FleetOfThings` class with a `main` method
+    // - Download those, use those
+    // - In the main method create a fleet
+    // - Achieve this output:
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+    Thing thing1 = new Thing("Get Milk", false);
+    Thing thing2 = new Thing("Remove the obstacles", false);
+    Thing thing3 = new Thing("Stand up", true);
+    Thing thing4 = new Thing("Eat lunch", true);
+
+    fleet.add(thing1);
+    fleet.add(thing2);
+    fleet.add(thing3);
+    fleet.add(thing4);
+
+    System.out.println(fleet);
+  }
+}
