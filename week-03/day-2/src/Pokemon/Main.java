8b3850e6b0d5e9759f13db4203e50aace5e3c363
Rita Csullag <csullagrita@gmail.com>
2020-07-28 10:40:44 +0200

    Add Pokemon task

---
+++ week-03/day-2/src/Pokemon/Main.java
@@ -0,0 +1,44 @@
+package Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<Pokemon> pokemonOfAsh = initializePokemons();
+
+    // Every pokemon has a name and a type.
+    // Certain types are effective against others, e.g. water is effective against fire.
+
+    // Ash has a few pokemon.
+    // A wild pokemon appeared!
+
+    Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
+
+    // Which pokemon should Ash use?
+    Pokemon choosedPokemon = null;
+    chooseOnePokemon(pokemonOfAsh, wildPokemon, choosedPokemon);
+  }
+
+  private static void chooseOnePokemon(List<Pokemon> pokemonOfAsh, Pokemon wildPokemon,
+                                       Pokemon choosedPokemon) {
+    for (Pokemon pokemon : pokemonOfAsh) {
+      if (pokemon.isEffectiveAgainst(wildPokemon)) {
+        choosedPokemon = pokemon;
+      }
+    }
+    System.out.print("I choose you, " + choosedPokemon.name);
+  }
+
+  private static List<Pokemon> initializePokemons() {
+    List<Pokemon> pokemon = new ArrayList<>();
+
+    pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
+    pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
+    pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
+    pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
+    pokemon.add(new Pokemon("Kingler", "water", "fire"));
+
+    return pokemon;
+  }
+}
