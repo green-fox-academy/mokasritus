da5cc4524af1d6e0f9c4f291fded3dfb3d2c3142
Rita Csullag <csullagrita@gmail.com>
2020-08-04 14:15:30 +0200

    Add Green Fox task Person and Student Class

---
+++ week-04/day-2/src/greenfox/Main.java
@@ -0,0 +1,19 @@
+package greenfox;
+
+public class Main {
+  public static void main(String[] args) {
+    Person person = new Person ();
+    person.getGoal();
+    person.introduce();
+
+    Person person1 = new Person("Boldizsar", 25, "male");
+    person1.introduce();
+
+    Student student = new Student();
+    student.introduce();
+
+    student.skipDays(3);
+    student.introduce();
+
+  }
+}
