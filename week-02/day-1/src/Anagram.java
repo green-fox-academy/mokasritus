1542f2b8b24ea11453c5a67e22d08daca3d8c53b
Rita Csullag <csullagrita@gmail.com>
2020-08-08 19:49:35 +0200

    Add Anagram and Unique task to week-02/day-1

---
+++ week-02/day-1/src/Anagram.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Anagram {
+  //Exercise
+  //Create a function named is anagram following your current language's style guide. It should take two strings and return a boolean value depending on whether its an anagram or not.
+  //
+  //Examples
+  //input 1	input 2	output
+  //"dog"	"god"	true
+  public static void main(String[] args) {
+    System.out.println(isAnagram("dog", "fod"));
+  }
+  public static boolean isAnagram(String input1, String input2){
+    char[] fromInput1=input1.toCharArray();
+    char[] sortedFromInput1;
+    Arrays.sort(fromInput1);
+    char[] fromInput2=input2.toCharArray();
+    Arrays.sort(fromInput2);
+    if (Arrays.equals(fromInput1, fromInput2))
+    {return true;}
+    else {return false;}
+  }
+}
---
+++ week-02/day-1/src/Unique.java
@@ -0,0 +1,26 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import java.util.Arrays;
+import org.w3c.dom.ls.LSOutput;
+
+public class Unique {
+  public static void main(String[] args) {
+    //  Create a function that takes a list of numbers as a parameter
+    //  Returns a list of numbers where every number in the list occurs only once
+
+    //  Example
+    System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    //  should print: `[1, 11, 34, 52, 61]`
+  }
+
+  public static String unique(int[] numbers) {
+    int[] everyNumberOnlyOnes = new int[numbers.length];
+    int i = 0;
+    for (int number : numbers) {
+      if (!Arrays.toString(everyNumberOnlyOnes).contains(Integer.toString(number))) {
+        everyNumberOnlyOnes[i] = number;
+        i++;
+      }
+    }
+    return Arrays.toString(Arrays.copyOf(everyNumberOnlyOnes, i));
+  }
+}
