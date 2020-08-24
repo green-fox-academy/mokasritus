1f3f56c6cafcb31685e53d10a57070453a139be9
Rita Csullag <csullagrita@gmail.com>
2020-08-24 14:29:16 +0200

    Add Exercise4 and Exercise5 task

--- week-06/day-1/src/exercises/Exercise4.java
+++ week-06/day-1/src/exercises/Exercise4.java
@@ -2,6 +2,7 @@ package exercises;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //Write a Stream Expression to get the average
 //value of the odd numbers from the following list:
@@ -11,6 +12,9 @@ public class Exercise4 {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
     numbers.stream()
-        
+        .filter(numb->numb%2!=0)
+        .mapToInt(numb->numb)
+        .average()
+        .ifPresent(System.out::println);
   }
 }
