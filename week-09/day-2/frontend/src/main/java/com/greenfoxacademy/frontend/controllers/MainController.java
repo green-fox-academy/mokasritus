559c845e5baf04bf3d76edd0cb36cb77d83c84a1
Rita Csullag <csullagrita@gmail.com>
2020-09-15 13:30:03 +0200

    Add frontend part

---
+++ week-09/day-2/frontend/src/main/java/com/greenfoxacademy/frontend/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.frontend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+  @GetMapping(value="/")
+  public String renderPage(){
+    return "index";
+  }
+}
