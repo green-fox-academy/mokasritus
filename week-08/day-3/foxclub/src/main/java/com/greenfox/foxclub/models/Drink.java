f304d28c38a0849737c850aa538583160e3c8319
Rita Csullag <csullagrita@gmail.com>
2020-09-24 11:35:04 +0200

    remove day-3 directory from week-08

--- week-08/day-3/foxclub/src/main/java/com/greenfox/foxclub/models/Drink.java
+++
@@ -1,8 +0,0 @@
-package com.greenfox.foxclub.models;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public enum Drink {
-  tea, water, chokolatemilk, palinka
-}
--- week-08/day-3/foxclub/src/main/java/com/greenfox/foxclub/models/Food.java
+++
@@ -1,4 +0,0 @@
-package com.greenfox.foxclub.models;
-
-public enum Food {onion, brownie, tomato}
-
